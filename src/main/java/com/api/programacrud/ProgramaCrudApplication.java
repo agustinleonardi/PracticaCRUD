88cfbd21918729d4db31fa672fe267d6addf657c
agustinleonardi <agustinleonardi5@gmail.com>
2024-03-11 10:58:39 -0300

    first commit

---
+++ src/main/java/com/api/programacrud/ProgramaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.api.programacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramaCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/programacrud/ProgramaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.programacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
